15e744b5168049e777a1552027582a8f4f29b018
Ujjawal kumar Rai <raidk084@gmail.com>
2025-11-21 05:38:29 +0530

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,11 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes)
+  ]
+}).catch((err: any) => console.error(err));
